73460cf3634cdd8f7b4dda1fccf4befaf49dcf4a
Romuald Rousseau <romualdrousseau@gmail.com>
2024-06-25 15:01:39 +0800

    refacto: cleanup

--- Tutorial2/tutorial2/pyflow/__init__.py
+++ Tutorial2/tutorial2/pyflow/__init__.py
@@ -1,5 +1,5 @@
-from .conv1d import *  # noqa: F403
-from .dense import *  # noqa: F403
-from .genetic.dense import *  # noqa: F403
-from .genetic.model import *  # noqa: F403
-from .sequential import *  # noqa: F403
+from tutorial2.pyflow.conv1d import *  # noqa: F403
+from tutorial2.pyflow.dense import *  # noqa: F403
+from tutorial2.pyflow.genetic.dense import *  # noqa: F403
+from tutorial2.pyflow.genetic.model import *  # noqa: F403
+from tutorial2.pyflow.sequential import *  # noqa: F403
